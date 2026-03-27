1a72302feebe81af73e6430391e934a43d77d418
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-03-27 06:05:47 +0200

    Clean up the wrapper

--- lc-framework-sys/wrapper.hpp
+++ lc-framework-sys/wrapper.hpp
@@ -2,8 +2,8 @@
 
 #include "lc.h"
 
-extern "C" int lc_compress(
-    const size_t npepros,
+extern "C" int
+lc_compress(const size_t npepros,
     const LC_CPUpreprocessor* const prepro_ids,
     const size_t* const prepros_nparams,
     const double* const prepros_params,
@@ -12,12 +12,27 @@ extern "C" int lc_compress(
     const byte* const input,
     const long long insize,
     byte** encoded,
-    long long* encsize
-) {
+    long long* encsize)
+{
     byte* hpreencdata = nullptr;
     byte* hencoded = nullptr;
 
     try {
+        if (ncomps > sizeof(long long)) {
+            fprintf(stderr, "ERROR: too many components\n\n");
+            throw std::runtime_error("LC error");
+        }
+
+        if (insize <= 0) {
+            fprintf(stderr, "ERROR: input too small\n\n");
+            throw std::runtime_error("LC error");
+        }
+        if (insize >= std::numeric_limits<long long>::max()) {
+            fprintf(stderr, "ERROR: input too large\n\n");
+            throw std::runtime_error("LC error");
+        }
+
+        // extract preprocessor list and their parameters
         std::vector<std::pair<byte, std::vector<double>>> prepros;
         size_t prepros_params_cnt = 0;
         for (auto i = 0; i < npepros; ++i) {
@@ -29,66 +44,59 @@ extern "C" int lc_compress(
             prepros.push_back(std::make_pair((byte)prepro_ids[i], params));
         }
 
-        std::vector<std::vector<byte>> comp_list;
-        for (auto i = 0; i < ncomps; ++i) {
-            std::vector<byte> comps = { (byte)comp_ids[i] };
-            comp_list.push_back(comps);
-        }
-        auto stages = comp_list.size();
-
-        if (insize <= 0) {
-            fprintf(stderr, "ERROR: input too small\n\n"); throw std::runtime_error("LC error");
-        }
-        if (insize >= std::numeric_limits<long long>::max()) {
-            fprintf(stderr, "ERROR: input too large\n\n"); throw std::runtime_error("LC error");
+        // create component chain for encoding
+        unsigned long long comp_chain = 0;
+        for (auto i = 0; i < ncomps; i++) {
+            comp_chain |= ((byte)comp_ids[i]) << (i * 8);
         }
 
-        // CPU preprocessor encoding
-        hpreencdata = new byte [insize];
+        // allocate CPU memory for preprocessed data
+        hpreencdata = new byte[insize];
         std::copy(input, input + insize, hpreencdata);
+
+        // encode with CPU preprocessors
         long long hpreencsize = insize;
         h_preprocess_encode(hpreencsize, hpreencdata, prepros);
 
-        // allocate CPU memory
-        const long long hchunks = (hpreencsize + CS - 1) / CS;  // round up
-        const long long hmaxsize = 2 * sizeof(long long) + hchunks * sizeof(short) + hchunks * CS;  //MB: adjust later
-        hencoded = new byte [hmaxsize];
+        // allocate CPU memory for encoded data
+        const long long hchunks = (hpreencsize + CS - 1) / CS; // round up
+        const long long hmaxsize = 2 * sizeof(long long) + hchunks * sizeof(short) + hchunks * CS; // MB: adjust later
+        hencoded = new byte[hmaxsize];
+
+        // encode with CPU component chain
         long long hencsize = 0;
+        h_encode(comp_chain, hpreencdata, hpreencsize, hencoded, hencsize);
 
-        // create chain for current combination and output
-        unsigned long long combin = 0;
-        unsigned long long chain = 0;
-        for (auto s = 0; s < stages; s++) {
-            unsigned long long compnum = comp_list[s][(combin >> (s * 8)) & 0xff];
-            chain |= compnum << (s * 8);
-        }
-
-        // CPU encoding
-        h_encode(chain, hpreencdata, hpreencsize, hencoded, hencsize);
-
-        delete [] hpreencdata;
+        // clean up and return encoded data
+        delete[] hpreencdata;
 
         *encoded = hencoded;
         *encsize = hencsize;
 
         return 0;
-    }
-    catch(const std::exception &ex) {
+    } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
 
-        if (hpreencdata != nullptr) delete [] hpreencdata;
-        if (hencoded != nullptr) delete [] hencoded;
+        // clean up, if necessary
+        if (hpreencdata != nullptr) {
+            delete[] hpreencdata;
+        }
+        if (hencoded != nullptr) {
+            delete[] hencoded;
+        }
 
         return -1;
     }
 }
 
-extern "C" void lc_free_bytes(byte* data) {
-    delete [] data;
+extern "C" void
+lc_free_bytes(byte* data)
+{
+    delete[] data;
 }
 
-extern "C" int lc_decompress(
-    const size_t npepros,
+extern "C" int
+lc_decompress(const size_t npepros,
     const LC_CPUpreprocessor* const prepro_ids,
     const size_t* const prepros_nparams,
     const double* const prepros_params,
@@ -97,13 +105,18 @@ extern "C" int lc_decompress(
     const byte* const encoded,
     const long long encsize,
     byte** decoded,
-    long long* decsize
-) {
+    long long* decsize)
+{
     byte* hencoded = nullptr;
     byte* hdecoded = nullptr;
-    byte* hpredecdata = nullptr;
 
     try {
+        if (ncomps > sizeof(long long)) {
+            fprintf(stderr, "ERROR: too many components\n\n");
+            throw std::runtime_error("LC error");
+        }
+
+        // extract preprocessor list and their parameters
         std::vector<std::pair<byte, std::vector<double>>> prepros;
         size_t prepros_params_cnt = 0;
         for (auto i = 0; i < npepros; ++i) {
@@ -115,58 +128,60 @@ extern "C" int lc_decompress(
             prepros.push_back(std::make_pair((byte)prepro_ids[i], params));
         }
 
-        std::vector<std::vector<byte>> comp_list;
-        for (auto i = 0; i < ncomps; ++i) {
-            std::vector<byte> comps = { (byte)comp_ids[i] };
-            comp_list.push_back(comps);
+        // create component chain for encoding
+        unsigned long long comp_chain = 0;
+        for (auto i = 0; i < ncomps; i++) {
+            comp_chain |= ((byte)comp_ids[i]) << (i * 8);
         }
-        auto stages = comp_list.size();
 
+        // create component chain for decoding
+        unsigned long long comp_schain = comp_chain;
+        if (comp_schain != 0) {
+            while ((comp_schain >> 56) == 0) {
+                comp_schain <<= 8;
+            }
+        }
+
+        // read the size of the preprocessed data
         long long pre_size = 0;
         assert(encsize >= sizeof(pre_size));
         std::memcpy(&pre_size, encoded, sizeof(pre_size));
 
-        // allocate CPU memory
-        hencoded = new byte [std::max(pre_size, encsize)];
+        // allocate CPU memory for decoded data
+        hencoded = new byte[std::max(pre_size, encsize)];
         std::copy(encoded, encoded + encsize, hencoded);
-        hdecoded = new byte [pre_size];
+        hdecoded = new byte[pre_size];
+
+        // decode with CPU component chain
         long long hdecsize = 0;
+        h_decode(comp_schain, hencoded, hdecoded, hdecsize);
 
-        // create chain for current combination and output
-        unsigned long long combin = 0;
-        unsigned long long chain = 0;
-        for (auto s = 0; s < stages; s++) {
-            unsigned long long compnum = comp_list[s][(combin >> (s * 8)) & 0xff];
-            chain |= compnum << (s * 8);
-        }
+        // reuse CPU memory of decoded data to postprocess
+        assert(hdecsize <= pre_size);
+        byte* hpredecdata = hdecoded;
 
-        // CPU decoding
-        unsigned long long schain = chain;
-        if (chain != 0) {
-          while ((schain >> 56) == 0) schain <<= 8;
-        }
-        h_decode(schain, encoded, hdecoded, hdecsize);
-
-        // CPU preprocessor decoding
-        hpredecdata = new byte [hdecsize];
-        std::copy(hdecoded, hdecoded + hdecsize, hpredecdata);
+        // decode with CPU preprocessors
         long long hpredecsize = hdecsize;
         h_preprocess_decode(hpredecsize, hpredecdata, prepros);
 
-        delete [] hencoded;
-        delete [] hdecoded;
+        // clean up and return decoded data
+        delete[] hencoded;
+        // hdecoded points to the same memory as hpredecdata
 
         *decoded = hpredecdata;
         *decsize = hpredecsize;
 
         return 0;
-    }
-    catch(const std::exception &ex) {
+    } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
 
-        if (hencoded != nullptr) delete [] hdecoded;
-        if (hdecoded != nullptr) delete [] hdecoded;
-        if (hpredecdata != nullptr) delete [] hpredecdata;
+        // clean up, if necessary
+        if (hencoded != nullptr) {
+            delete[] hencoded;
+        }
+        if (hdecoded != nullptr) {
+            delete[] hdecoded;
+        }
 
         return -1;
     }
